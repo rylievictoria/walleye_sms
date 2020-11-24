8d11bc4eb491a6a635c26f07fea3a091e07ccfeb
rhendren <rhendren@better.com>
2020-11-23 21:51:30 -0500

    First build

---
+++ build/precache-manifest.82527516b507cb6354f4281244d29960.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445f161b3273f4a3ef5e65d00b8f63f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adeaa66fe7051a22cf04",
+    "url": "/static/css/main.84f8b402.chunk.css"
+  },
+  {
+    "revision": "1a8e6c1e9f4f76d5eed4",
+    "url": "/static/js/2.7834bb5d.chunk.js"
+  },
+  {
+    "revision": "31c2772b83dc7ae3b9479c64849739e0",
+    "url": "/static/js/2.7834bb5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adeaa66fe7051a22cf04",
+    "url": "/static/js/main.094b6ea3.chunk.js"
+  },
+  {
+    "revision": "ab778565ea0c6bdc8c2a",
+    "url": "/static/js/runtime-main.1eed738d.js"
+  }
+]);
